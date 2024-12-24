24e5fd0352fd217776707c91a03d4c0c54b3cbff
Alessandro <aberti77@gmail.com>
2024-12-24 19:25:39 +0100

    agg
    Aggiorna Home.razor e aggiungi classe Vehicle
    
    - Modificato Home.razor: aggiornato titolo a "OD533" e tradotto il messaggio di benvenuto in italiano.
    - Aggiunto file binario `Screenshot_1.png`.
    - Aggiunta classe `Vehicle` in `Vehicle.cs` con proprietà e annotazioni di dati per la validazione.

---
+++ Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OD533_BlazorApp.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Make is required")]
+        public required string Make { get; set; }
+
+        [Required(ErrorMessage = "Model is required")]
+        public required string Model { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Year must be a positive number")]
+        public int Year { get; set; }
+
+        public string? LicensePlate { get; set; }
+    }
+}
